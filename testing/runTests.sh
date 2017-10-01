1139cd535cca40af1ada6afa36fd98325c326c6f
Veronica K. B. Olsen <code@vkbo.net>
2017-10-01 19:39:28 +0200

    Updated testing script

--- testing/runTests.sh
+++ testing/runTests.sh
@@ -3,58 +3,40 @@
 # echo -n "........................................................................"
 # echo -n "................................................................. Passed"
 
-echo -n "1/2  Testing: makeNovel FODT to FODT ............................ "
+IND="     "
+
+echo -n " 1/2  Testing: makeNovel FODT to FODT ........................... "
 cd fodt
 ../../makeNovel.py -i novel.cnf -f FODT -d DEBUG -q -l novel-fodt.log
 LOGDIFF=$(diff -q novel-fodt.log novel-fodt.log.reference)
 LOGRES=$?
 OUTDIFF=$(diff -q novel.fodt novel.fodt.reference)
 OUTRES=$?
-if [ $LOGRES -eq 0 ] && [ $OUTRES -eq 0 ]; then
-    echo "Passed"
-    if [ $LOGRES -eq 0 ]; then
-        echo "     Log file OK"
-    fi
-    if [ $OUTRES -eq 0 ]; then
-        echo "     FODT output file OK"
-    fi
-else
-    echo "Failed"
-    if [ $LOGRES -gt 0 ]; then
-        echo "     Reason:  $LOGDIFF"
-        diff novel-fodt.log novel-fodt.log.reference
-    fi
-    if [ $OUTRES -gt 0 ]; then
-        echo "     Reason:  $OUTDIFF"
-        diff novel.fodt novel.fodt.reference
-    fi
+if [ $LOGRES -eq 0 ] && [ $OUTRES -eq 0 ]; then echo "Passed"; else echo "Failed"; fi
+if [ $LOGRES -eq 0 ]; then echo "$IND Log file OK"; else
+    echo "$IND Reason:  $LOGDIFF"
+    diff novel-fodt.log novel-fodt.log.reference
+fi
+if [ $OUTRES -eq 0 ]; then echo "$IND FODT output file OK"; else
+    echo "$IND Reason:  $OUTDIFF"
+    diff novel.fodt novel.fodt.reference
 fi
 cd ..
 
-echo -n "2/2  Testing: makeNovel TXT to FODT ............................. "
+echo -n " 2/2  Testing: makeNovel TXT to FODT ............................ "
 cd txt
 ../../makeNovel.py -i novel.cnf -f FODT -d DEBUG -q -l novel-fodt.log
 LOGDIFF=$(diff -q novel-fodt.log novel-fodt.log.reference)
 LOGRES=$?
 OUTDIFF=$(diff -q novel.fodt novel.fodt.reference)
 OUTRES=$?
-if [ $LOGRES -eq 0 ] && [ $OUTRES -eq 0 ]; then
-    echo "Passed"
-    if [ $LOGRES -eq 0 ]; then
-        echo "     Log file OK"
-    fi
-    if [ $OUTRES -eq 0 ]; then
-        echo "     FODT output file OK"
-    fi
-else
-    echo "Failed"
-    if [ $LOGRES -gt 0 ]; then
-        echo "     Reason:  $LOGDIFF"
-        diff novel-fodt.log novel-fodt.log.reference
-    fi
-    if [ $OUTRES -gt 0 ]; then
-        echo "     Reason:  $OUTDIFF"
-        diff novel.fodt novel.fodt.reference
-    fi
+if [ $LOGRES -eq 0 ] && [ $OUTRES -eq 0 ]; then echo "Passed"; else echo "Failed"; fi
+if [ $LOGRES -eq 0 ]; then echo "$IND Log file OK"; else
+    echo "$IND Reason:  $LOGDIFF"
+    diff novel-fodt.log novel-fodt.log.reference
+fi
+if [ $OUTRES -eq 0 ]; then echo "$IND FODT output file OK"; else
+    echo "$IND Reason:  $OUTDIFF"
+    diff novel.fodt novel.fodt.reference
 fi
 cd ..
